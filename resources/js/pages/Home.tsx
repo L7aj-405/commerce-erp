6e3f7217fc6ea12aa5c74aa60f48d7ed255cdc93
Mohamed <mohamed.agday.ss@gmail.com>
2026-08-23 15:39:55 +0100

    installation

---
+++ resources/js/pages/Home.tsx
@@ -0,0 +1,14 @@
+import { Head } from '@inertiajs/react';
+
+export default function Home() {
+    return (
+        <>
+            <Head title="Commerce ERP" />
+
+            <main>
+                <h1>Commerce ERP</h1>
+                <p>Laravel + React + Inertia + TypeScript + Vite is working.</p>
+            </main>
+        </>
+    );
+}
